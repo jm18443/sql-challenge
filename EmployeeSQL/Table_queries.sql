fb4c36e7741f77ea59aa060b00859ddfa0b11cbe
jm18443 <59849130+jm18443@users.noreply.github.com>
2022-11-14 09:57:52 -0500

    Update Table_queries.sql

--- EmployeeSQL/Table_queries.sql
+++ EmployeeSQL/Table_queries.sql
@@ -1,43 +1,38 @@
---1
-select e.emp_no,last_name,first_name,sex,salary 
-from employees as e, salaries as s
-where e.emp_no = s.emp_no
-order by emp_no; 
+CREATE TABLE departments(
+dept_no varchar(200) NOT NULL PRIMARY KEY,
+dept_name varchar(200)
+);
 
---2
-select first_name, last_name,hire_date 
-from employees
-where hire_date between '1986-01-01' and '1986-12-31';
+CREATE TABLE dept_emp(
+emp_no int NOT NULL,
+dept_no varchar(200),
+FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
 
---3
-select m.dept_no,dept_name,m.emp_no,last_name,first_name
-from dept_manager as m, departments as d, employees as e
-where e.emp_no = m.emp_no and d.dept_no = m.dept_no;
 
---4
-select x.dept_no,e.emp_no,last_name,first_name,dept_name
-from departments as d, employees as e, dept_emp as x
-where d.dept_no = x.dept_no and e.emp_no = x.emp_no
-order by e.emp_no;
 
---5
-select first_name,last_name,sex
-from employees
-where first_name = 'Hercules' and last_name like 'B%'
-order by last_name;
+CREATE TABLE dept_manager(
+dept_no varchar(200) NOT NULL,
+emp_no int
+);
 
---6
-select e.emp_no,last_name,first_name
-from employees as e, departments as d, dept_emp as x
-where d.dept_name = 'Sales' and e.emp_no = x.emp_no and x.dept_no = d.dept_no;
+CREATE TABLE employees(
+emp_no int NOT NULL PRIMARY KEY,
+emp_title varchar(200),
+birth_date date,
+first_name varchar(200),
+last_name varchar(200),
+sex varchar(200),
+hire_date date
+);
 
---7
-select e.emp_no,last_name,first_name,dept_name
-from departments as d, employees as e, dept_emp as x
-where (d.dept_name = 'Sales' or d.dept_name = 'Development') and e.emp_no = x.emp_no and x.dept_no = d.dept_no;
+CREATE TABLE salaries(
+emp_no int NOT NULL PRIMARY KEY ,
+salary int,
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
 
---8
-select last_name,count(last_name) as "Last name frequency"
-from employees as e
-group by last_name
-order by "Last name frequency" desc;
+CREATE TABLE titles(
+title_id varchar(200) NOT NULL PRIMARY KEY ,
+title varchar(200)
+);
